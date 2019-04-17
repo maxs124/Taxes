f0a1235ba48524076359975698705bfeb9cbe1f3
BuildTools <unconfigured@null.spigotmc.org>
2019-04-17 10:59:23 -0700

    finished formula, and started with vaadin grid (averted major crisis phew(deleting the whole thing))

--- src/main/kotlin/com/mshmelev/taxes/view/MainView.kt
+++ src/main/kotlin/com/mshmelev/taxes/view/MainView.kt
@@ -2,7 +2,9 @@ package com.mshmelev.taxes.view
 
 import com.mshmelev.taxes.db.repository.IrsDataRepository
 import com.mshmelev.taxes.service.FinderService
+import com.mshmelev.taxes.utils.CalculatedBudget
 import com.mshmelev.taxes.utils.calculator
+import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.html.Label
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.textfield.TextField
@@ -17,8 +19,10 @@ import com.vaadin.flow.spring.annotation.UIScope
 class MainView(private val finderService: FinderService,
                private val irsDataRepository: IrsDataRepository) : VerticalLayout() {
     private fun initialize() {
-        val message = Label()
+        val errorText = Label()
+        val grid = Grid(CalculatedBudget::class.java)
         val textField = TextField("Zip Code", "Zip Code")
+        textField.width = "100%"
 //      textField.addValueChangeListener { event -> message.text = "${finderService.getTaxPaidByZipCode(event.value)} Dollars" }
 
         textField.addValueChangeListener { event ->
@@ -26,18 +30,19 @@ class MainView(private val finderService: FinderService,
             val budget = finderService.getBudget()
 
             if (paidTax.toInt() == 0 || event.value.toInt() == 0) {
-                message.text = "ERROR: Enter a Valid Zip Code"
+                errorText.text = "ERROR: Enter a Valid Zip Code"
             } else {
                 val allTaxes = irsDataRepository.findAll()
                 val zipTaxes = irsDataRepository.findAllByZipCode(event.value)
-                message.text = "${calculator(allTaxes, zipTaxes, budget)}"
+                val calculatedTax: List<CalculatedBudget> = calculator(allTaxes, zipTaxes, budget)
+                errorText.text = "${calculator(allTaxes, zipTaxes, budget)}"
+                grid.setSizeFull()
+                grid.setItems(calculatedTax)
+
             }
 
         }
-
-
-        add(textField, message)
-
+        add(textField, errorText, grid)
     }
 
     init {
